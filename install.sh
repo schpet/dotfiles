3279f1db328a4c7b64edcd1c97e1bea107fa1903
Peter Schilling <peter@peterschilling.org>
2021-11-30 03:31:53 +0000

    cleanup

--- install.sh
+++ install.sh
@@ -23,8 +23,9 @@ sudo apt update
 sudo apt install gh
 
 # ripgrep
-curl -sLO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb \
-    && sudo dpkg -i ripgrep_13.0.0_amd64.deb
+curl -sLO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
+sudo dpkg -i ripgrep_13.0.0_amd64.deb
+rm ripgrep_13.0.0_amd64.deb
 
 # heroku cli
 curl https://cli-assets.heroku.com/install-ubuntu.sh | sh
