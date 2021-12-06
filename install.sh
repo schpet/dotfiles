311aa74e88943e49ca789b293f9d18c9048feab8
Peter Schilling <peter@peterschilling.org>
2021-12-06 07:35:00 -0800

    os specific dep install script

--- install.sh
+++ install.sh
@@ -11,21 +11,38 @@ fi
 cp .gitconfig ~/.gitconfig
 
 # TODO: following stuff is all linux, need to figure out mac
+case "$(uname -s)" in
 
-sudo apt-get update
-export DEBIAN_FRONTEND=noninteractive
-sudo apt-get -y install --no-install-recommends bat vim stow
+	Darwin)
+		echo 'todo: mac os x setup, install homebrew etc'
+		;;
 
-# gh cli
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-sudo apt update
-sudo apt install gh
+	Linux)
+		sudo apt-get update
+		export DEBIAN_FRONTEND=noninteractive
+		sudo apt-get -y install --no-install-recommends bat vim stow
 
-# ripgrep
-curl -sLO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
-sudo dpkg -i ripgrep_13.0.0_amd64.deb
-rm ripgrep_13.0.0_amd64.deb
+		# gh cli
+		curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+		echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+		sudo apt update
+		sudo apt install gh
 
-# heroku cli
-curl https://cli-assets.heroku.com/install-ubuntu.sh | sh
+		# ripgrep
+		curl -sLO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
+		sudo dpkg -i ripgrep_13.0.0_amd64.deb
+		rm ripgrep_13.0.0_amd64.deb
+
+		# heroku cli
+		curl https://cli-assets.heroku.com/install-ubuntu.sh | sh
+     ;;
+
+   CYGWIN*|MINGW32*|MSYS*|MINGW*)
+     echo 'todo: windows'
+     ;;
+
+   *)
+     echo "Unknown os: $(uname -s)"
+     ;;
+esac
+
