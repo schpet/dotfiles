1ff42713c5b903252ef30556496744897eeb2b8f
Peter Schilling <peter@peterschilling.org>
2021-12-14 21:49:28 -0800

    jq!!

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ case "$(uname -s)" in
 
 		sudo apt-get update
 		export DEBIAN_FRONTEND=noninteractive
-		sudo apt-get -y install --no-install-recommends bat stow
+		sudo apt-get -y install --no-install-recommends bat stow jq
 
 		# gh cli
 		curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
