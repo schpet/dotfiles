707a96cbad6a52c70f7fb0bfc9b7dadcd7fe0706
Peter Schilling <peter@tanookilabs.com>
2026-01-28 21:36:23 -0800

    Remove Linux dependency installation from install.sh

--- install.sh
+++ install.sh
@@ -10,31 +10,8 @@ case "$(uname -s)" in
 		;;
 
 	Linux)
-		echo "installing linux deps!"
-
-		sudo apt-get update
-		export DEBIAN_FRONTEND=noninteractive
-		sudo apt-get -y install --no-install-recommends bat stow jq
-
-		# gh cli
-		curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
-		echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-		sudo apt update
-		sudo apt install gh
-
-		# ripgrep
-		curl -sLO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
-		sudo dpkg -i ripgrep_13.0.0_amd64.deb
-		rm ripgrep_13.0.0_amd64.deb
-
-		# heroku cli
-		curl https://cli-assets.heroku.com/install-ubuntu.sh | sh
-
-		# starship
-		curl -fsSL https://starship.rs/install.sh > starship.sh
-		sh starship.sh -f
-		rm starship.sh
-     ;;
+		echo 'todo: linux setup'
+		;;
 
    CYGWIN*|MINGW32*|MSYS*|MINGW*)
      echo 'todo: windows'
